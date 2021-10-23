c48a43d6c5facab3b88681033395d8d46053175e
SzymonKonieczny <kowladMC@gmail.com>
2021-10-23 15:35:52 +0200

    itemsys

--- Assets/Scripts/ScriptableOjbectGens/Item.cs
+++ Assets/Scripts/ScriptableOjbectGens/Item.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public enum TYPE {GUN, FOOD };
+[CreateAssetMenu(fileName = "New Hun", menuName = "Item/Item")]
 public class Item : ScriptableObject
 {
  
     public TYPE ITEM_TYPE;
     public float EquipTime;
     public GameObject Prefab;
+    public GameObject Prefab_Laying;
     public Sprite Icon;
 
 
